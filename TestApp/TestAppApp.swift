e401c4e7a43f3ed7b5e8110a67b54b9e8e93bacd
Vadiraj Hippargi <Vadi@leadconsultant.com>
2020-12-09 20:46:49 -0800

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Vadiraj Hippargi on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
